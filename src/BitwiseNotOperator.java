0b242a3956d050334812e0f9454fa000e3ca3ced
PankajMoon <pankajmoon26@gmail.com>
2022-09-18 20:41:10 +0530

    First Commit

---
+++ src/BitwiseNotOperator.java
@@ -0,0 +1,14 @@
+
+public class BitwiseNotOperator {
+	
+		 public static void main(String[] args)	 {
+			 
+		 int i = 50;
+		 System.out.print(~i);
+		 System.out.print(",");
+		 System.out.print(~--i);
+		 System.out.print(",");
+		 System.out.print(~++i);
+		 }
+		}
+
